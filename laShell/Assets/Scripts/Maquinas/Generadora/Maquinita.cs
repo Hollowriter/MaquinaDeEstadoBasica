d8772f42a8584333b1be1ac30b34d5db09155b67
Hollowriter <deadgamergx2@hotmail.com.ar>
2018-08-13 22:09:08 -0300

    primer commit

---
+++ laShell/Assets/Scripts/Maquinas/Generadora/Maquinita.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maquinita  // Clase maquina de estado (Con esto creas maquinas de estado)
+{
+    int[,] maquinita;
+    int state;
+
+    public void Init(int statesCount, int eventCount)
+    {
+        maquinita = new int[statesCount, eventCount];
+
+        for (int i = 0; i < statesCount; i++)
+        {
+            for (int o = 0; o < eventCount; o++)
+            {
+                maquinita[i, o] = -1;
+            }
+        }
+    }
+
+    public void SetRelation(int srcState, int evt, int dsrState)
+    {
+        maquinita[srcState, evt] = dsrState;
+    }
+
+    public int GetState()
+    {
+        return state;
+    }
+
+    public void SetEvent(int evt)
+    {
+        if (maquinita[state, evt] != -1)
+        {
+            state = maquinita[state, evt];
+        }
+    }
+}
---
+++ laShell/Assets/Scripts/Maquinas/ShellMaquinas/WorkerMaquinita.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerMaquinita : MonoBehaviour {
+    Maquinita maq = new Maquinita();
+    int state;
+    // additional variables
+    int objectsCarried;
+
+    enum States
+    {
+        Idle,
+        ToMine,
+        Mining,
+        ToDeposit
+    };
+
+    enum Events
+    {
+        Ordered,
+        OnMine,
+        Full,
+        Deposit,
+        NoMine
+    };
+
+    void Start ()
+    {
+        state = -1;
+        objectsCarried = 0;
+        maq.SetRelation((int)States.Idle, (int)Events.Ordered, (int)States.ToMine);
+        maq.SetRelation((int)States.ToMine, (int)Events.OnMine, (int)States.Mining);
+        maq.SetRelation((int)States.Mining, (int)Events.Full, (int)States.ToDeposit);
+        maq.SetRelation((int)States.ToMine, (int)Events.Full, (int)States.ToDeposit);
+        maq.SetRelation((int)States.ToDeposit, (int)Events.Deposit, (int)States.ToMine);
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
