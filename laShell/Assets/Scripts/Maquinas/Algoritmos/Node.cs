c61dc9bd3c61051fe2bcfce263ddbf7d010cfc5f
unknown <pehuen-testa@hotmail.com>
2018-09-03 21:58:47 -0300

    avanzando con el pathfinding

---
+++ laShell/Assets/Scripts/Maquinas/Algoritmos/Node.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    List<Node> obj = new List<Node>();
+    // List<Node> path = new List<Node>(); // A pathfinder
+    float cost = 1;
+    float totalCost = 0;
+    bool isOpen = false;
+    bool isClosed = false;
+    bool isDestiny = false;
+    Node theParent = null;
+
+    public void AddAdjacent(Node n)
+    {
+        if (!obj.Contains(n))
+            obj.Add(n);
+    }
+
+   /* public void OpenNode(Node n, Node parent)
+    {
+        if(parent == null)
+        {
+            theParent = null;
+        }
+
+        if(n.GetOpen() == false && n.GetClosed() == false)
+        {
+            n.SetOpen(true);
+
+            if(isDestiny)
+            {
+                obj.Clear();
+                GetPath();
+            }
+            else
+            {
+                n.SetClosed(true);
+                for(int i = 0; i < obj.Count; i++)
+                {
+                    OpenNode(obj[i], this);
+                }
+            }
+        }
+    }*/ // A pathfinder
+
+   /* public void SelectNode()
+    {
+
+    }*/ // A pathfinder
+
+    public void SetOpen(bool open)
+    {
+        isOpen = open;
+    }
+
+    public void SetClosed(bool closed)
+    {
+        isClosed = closed;
+    }
+
+    public void SetDestiny(bool destiny)
+    {
+        isDestiny = destiny;
+    }
+
+    public bool GetOpen()
+    {
+        return isOpen;
+    }
+
+    public bool GetClosed()
+    {
+        return isClosed;
+    }
+
+    /*public List<Node> GetPath()
+    {
+        return path;
+    }*/ // A pathfinder
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(this.transform.position, Vector3.one);
+    }
+
+
+}
---
+++ laShell/Assets/Scripts/Maquinas/Algoritmos/NodeCreator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeCreator : MonoBehaviour
+{
+    public int Cols = 10;
+    public int Rows = 10;
+
+    Node[,] nodes;
+
+	// Use this for initialization
+	void Start ()
+    {
+        nodes = new Node[Cols, Rows];
+
+        for(int row = 0; row < Rows; row++)
+        {
+            for(int col = 0; col < Cols; col++)
+            {
+                GameObject go = new GameObject("Nodo");
+                nodes[col, row] = go.AddComponent<Node>();
+                go.transform.position = new Vector3(col * 2.0f, 0.0f, row * 2.0f);
+            }
+        }
+
+        for(int row = 0; row < Rows; row++)
+        {
+            for(int col = 0; col < Cols; col++)
+            {
+                if(col > 0)
+                {
+                    nodes[col - 1, row].AddAdjacent(nodes[col, row]);
+                    nodes[col, row].AddAdjacent(nodes[col-1, row]);
+                }
+
+                if(row > 0)
+                {
+                    nodes[col, row - 1].AddAdjacent(nodes[col, row]);
+                    nodes[col, row].AddAdjacent(nodes[col, row - 1]);
+                }
+
+                if (col < Cols - 1)
+                {
+                    nodes[col + 1, row].AddAdjacent(nodes[col, row]);
+                    nodes[col, row].AddAdjacent(nodes[col + 1, row]);
+                }
+
+                if(row < Rows- 1)
+                {
+                    nodes[col, row + 1].AddAdjacent(nodes[col, row]);
+                    nodes[col, row].AddAdjacent(nodes[col, row + 1]);
+                }
+            }
+        }
+    }
+}
