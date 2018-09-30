029ed5f8f9c915f8e30c8dbcde07b91867790342
Hollowriter <deadgamergx2@hotmail.com.ar>
2018-09-30 19:06:08 -0300

    tratando de agregar a* en el ejercicio del minero

---
+++ laShell/Assets/Scripts/Maquinas/ShellMaquinas/WorkerStarMaquinita.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerStarMaquinita : MonoBehaviour {
+    Maquinita maq = new Maquinita();
+    int state;
+    // additional variables
+    int objectsCarried;
+    float timer;
+    const float speedObj = 3.25f;
+    const float limitTimer = 2;
+    const int handTaker = 10;
+    int id;
+    bool movingToMine = false;
+    Vector3 destiny;
+    PathfinderStar pathfinder;
+    List<Node> thePathMine;
+    List<Node> thePathWarehouse;
+    [SerializeField]
+    NodeCreator nodes;
+    [SerializeField]
+    GameObject mine;
+    [SerializeField]
+    GameObject warehouse;
+
+    enum States
+    {
+        Idle,
+        ToMine,
+        Mining,
+        ToDeposit,
+        LastTrip
+    };
+
+    enum Events
+    {
+        Ordered,
+        OnMine,
+        Full,
+        Deposit,
+        NoMine
+    };
+
+    void Start()
+    {
+        Init();
+        pathfinder = GetComponent<PathfinderStar>();
+        thePathMine = pathfinder.GetPath(nodes.GetNodeByPosition(transform.position), nodes.GetNodeByPosition(mine.transform.position));
+        thePathWarehouse = pathfinder.GetPath(nodes.GetNodeByPosition(transform.position), nodes.GetNodeByPosition(warehouse.transform.position));
+        id = 0;
+        destiny = Vector3.zero;
+    }
+
+    void Update()
+    {
+        Behaviour();
+    }
+    void Init()
+    {
+        state = -1;
+        objectsCarried = 0;
+        timer = 2;
+        maq.Init(5, 5);
+        maq.SetRelation((int)States.Idle, (int)Events.Ordered, (int)States.ToMine);
+        maq.SetRelation((int)States.ToMine, (int)Events.OnMine, (int)States.Mining);
+        maq.SetRelation((int)States.Mining, (int)Events.Full, (int)States.ToDeposit);
+        maq.SetRelation((int)States.ToMine, (int)Events.Full, (int)States.ToDeposit);
+        maq.SetRelation((int)States.ToDeposit, (int)Events.Deposit, (int)States.ToMine);
+        maq.SetRelation((int)States.ToMine, (int)Events.NoMine, (int)States.LastTrip);
+        maq.SetRelation((int)States.Mining, (int)Events.NoMine, (int)States.LastTrip);
+        maq.SetRelation((int)States.LastTrip, (int)Events.Deposit, (int)States.Idle);
+    }
+    void Behaviour()
+    {
+        state = maq.GetState();
+        Ordering();
+        switch (state)
+        {
+            case (int)States.ToMine:
+                ToMine();
+                Debug.Log("asd");
+                movingToMine = true;
+                break;
+            case (int)States.ToDeposit:
+                ToDeposit();
+                movingToMine = false;
+                break;
+            case (int)States.Mining:
+                Mining();
+                movingToMine = true;
+                break;
+        }
+        NoMine();
+    }
+    void Ordering()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            maq.SetEvent((int)Events.Ordered);
+        }
+    }
+    void ToMine()
+    {
+        // transform.position = Vector3.MoveTowards(transform.position, mine.gameObject.transform.position, speedObj);
+        float step = speedObj * Time.deltaTime;
+        if (id < thePathMine.Count)
+        {
+            destiny = thePathMine[id].transform.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, destiny, step);
+        if ((destiny - transform.position).magnitude < 0.25f)
+        {
+            id++;
+        }
+    }
+    void ToDeposit()
+    {
+        // transform.position = Vector3.MoveTowards(transform.position, warehouse.gameObject.transform.position, speedObj);
+        float step = speedObj * Time.deltaTime;
+        if (id < thePathWarehouse.Count)
+        {
+            destiny = thePathWarehouse[id].transform.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, destiny, step);
+        if ((destiny - transform.position).magnitude < 0.25f)
+        {
+            id++;
+        }
+    }
+    void Mining()
+    {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if (timer >= limitTimer)
+        {
+            mine.GetComponent<MineMaquinita>().SetMinerals(mine.GetComponent<MineMaquinita>().GetMinerals() - handTaker);
+            objectsCarried += handTaker;
+            timer = 0;
+            Debug.Log("EnterMission");
+        }
+        if (objectsCarried >= 100)
+        {
+            maq.SetEvent((int)Events.Full);
+        }
+    }
+    void NoMine()
+    {
+        if (mine.GetComponent<MineMaquinita>().GetMinerals() <= 0)
+        {
+            maq.SetEvent((int)Events.NoMine);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "gold")
+        {
+            Debug.Log("colisiona");
+            maq.SetEvent((int)Events.OnMine);
+            transform.position = Vector3.MoveTowards(transform.position, mine.gameObject.transform.position, 0);
+            if (movingToMine == true)
+            {
+               // id = 0;
+            }
+        }
+        if (other.gameObject.tag == "warehouse")
+        {
+            Debug.Log("entra");
+            maq.SetEvent((int)Events.Deposit);
+            objectsCarried = 0;
+            if (movingToMine == false)
+            {
+              //  id = 0;
+            }
+        }
+    }
+}
---
+++ laShell/Assets/Scripts/Objetos/ObjetosBasicos/PlayerStar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStar : MonoBehaviour
+{
+    [SerializeField]
+    NodeCreator nodeCreator;
+    PathfinderStar pathfinder;
+    List<Node> thePath = new List<Node>();
+    [SerializeField]
+    GameObject mine;
+    int id;
+    const float speed = 3.25f;
+    Vector3 destiny;
+
+    void Start()
+    {
+        pathfinder = GetComponent<PathfinderStar>();
+        thePath = pathfinder.GetPath(nodeCreator.GetNodeByPosition(transform.position), nodeCreator.GetNodeByPosition(mine.transform.position));
+        id = 0;
+        destiny = Vector3.zero;
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        if (id < thePath.Count)
+        {
+            destiny = thePath[id].transform.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, destiny, step);
+
+        if ((destiny - transform.position).magnitude < 0.25f)
+        {
+            id++;
+        }
+    }
+}
