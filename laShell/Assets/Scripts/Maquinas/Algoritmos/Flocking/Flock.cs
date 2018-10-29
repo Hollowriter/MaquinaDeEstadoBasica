c8b9423f639f9ec1ba571b6c67af7adb59d41b03
Hollowriter <deadgamergx2@hotmail.com.ar>
2018-10-29 15:00:37 -0300

    pequeños cambios

---
+++ laShell/Assets/Scripts/Maquinas/Algoritmos/Flocking/Flock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour {
+    List<Boid> boidsList;
+	void Start () {
+        boidsList = new List<Boid>();
+	}
+	
+	void Update () {
+		
+	}
+}
