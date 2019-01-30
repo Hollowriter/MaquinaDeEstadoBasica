8150721ae0d462cd5837bf7bc1fa88b68aa3b9d0
Hollowriter <deadgamergx2@hotmail.com.ar>
2019-01-30 16:57:54 -0300

    generando boids para flocking

---
+++ laShell/Assets/Scripts/Maquinas/Algoritmos/Flocking/TheFlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheFlock : MonoBehaviour
+{
+
+    public GameObject boids;
+    static int numberOfBoids;
+    public static int spaceSize;
+    public static GameObject[] theBoids;
+
+	void Start ()
+    {
+        numberOfBoids = 10;
+        spaceSize = 5;
+        theBoids = new GameObject[numberOfBoids];
+        for (int i = 0; i < numberOfBoids; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-spaceSize, spaceSize),
+                                      Random.Range(-spaceSize, spaceSize),
+                                      Random.Range(-spaceSize, spaceSize));
+            theBoids[i] = (GameObject)Instantiate(boids, pos, Quaternion.identity);
+        }
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
