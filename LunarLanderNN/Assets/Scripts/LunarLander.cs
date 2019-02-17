d3e8fd2abe02c91217e16d0231fd7f932c9487a2
Hollowriter <deadgamergx2@hotmail.com.ar>
2019-02-17 19:40:17 -0300

    lunar lander mejorado

--- LunarLanderNN/Assets/Scripts/LunarLander.cs
+++ LunarLanderNN/Assets/Scripts/LunarLander.cs
@@ -29,44 +29,90 @@ public class LunarLander : LunarLanderBase
         }
         int scoreX = 0;
         int scoreY = 0;
+        int scoreSpeedX = 1;
+        int scoreSpeedY = 1;
+        if (lunarSpeed.x < 0)
+        {
+            if ((lunarSpeed.x * -1) < 0.5f)
+            {
+                scoreSpeedX = 2;
+            }
+            else
+            {
+                scoreSpeedX = 1;
+            }
+        }
+        else
+        {
+            if (lunarSpeed.x < 0.5f)
+            {
+                scoreSpeedX = 2;
+            }
+            else
+            {
+                scoreSpeedX = 1;
+            }
+        }
+        if (lunarSpeed.y < 0)
+        {
+            if ((lunarSpeed.y * -1) < 0.5f)
+            {
+                scoreSpeedY = 5;
+            }
+            else
+            {
+                scoreSpeedY = 1;
+            }
+        }
+        else
+        {
+            if (lunarSpeed.y < 0.5f)
+            {
+                scoreSpeedY = 5;
+            }
+            else
+            {
+                scoreSpeedY = 1;
+            }
+        }
         if (distanceToPlatform.x < 1)
         {
-            scoreX = 2;
+            scoreX = 2 * scoreSpeedX;
             if (distanceToPlatform.x < 0.8)
             {
-                scoreX = 4;
+                scoreX = 4 * scoreSpeedX;
             }
             if (distanceToPlatform.x < 0.4)
             {
-                scoreX = 8;
+                scoreX = 8 * scoreSpeedX;
             }
             if (distanceToPlatform.x < 0.2)
             {
-                scoreX = 16;
+                scoreX = 16 * scoreSpeedX;
             }
             if (distanceToPlatform.x == 0)
             {
-                scoreX = 32;
+                scoreX = 32 * scoreSpeedX;
             }
         }
         if (distanceToPlatform.y < 1)
         {
-            scoreY = 2;
+            scoreY = 2 * scoreSpeedY;
             if (distanceToPlatform.y < 0.8)
             {
-                scoreY = 4;
+                scoreY = 4 * scoreSpeedY;
             }
             if (distanceToPlatform.y < 0.4)
             {
-                scoreY = 8;
+                scoreY = 8 * scoreSpeedY;
             }
             if (distanceToPlatform.y < 0.2)
             {
-                scoreY = 16;
+                scoreY = 16 * scoreSpeedY;
             }
             if (distanceToPlatform.y == 0)
             {
-                scoreY = 32;
+                scoreY = 32 * scoreSpeedY;
             }
         }
         // Debug.Log("scX " + scoreX);
