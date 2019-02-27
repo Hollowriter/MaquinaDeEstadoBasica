4d997b152f4d3000f3dc79628c90255149725625
Hollowriter <deadgamergx2@hotmail.com.ar>
2019-02-26 23:15:02 -0300

    behaviour trees funcionando

---
+++ laShell/Assets/Scripts/Maquinas/Algoritmos/BehaviourTree/ActionNode.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionNode : BNode
+{
+    public delegate int ActionNodeDelegate();
+
+    private ActionNodeDelegate m_action;
+
+    public ActionNode(ActionNodeDelegate action)
+    {
+        m_action = action;
+    }
+
+    public override int Evaluate()
+    {
+        switch (m_action())
+        {
+            case 0:
+                m_nodeState = 0;
+                return m_nodeState;
+            case 1:
+                m_nodeState = 1;
+                return m_nodeState;
+            case 2:
+                m_nodeState = 2;
+                return m_nodeState;
+            default:
+                m_nodeState = 1;
+                return m_nodeState;
+        }
+    }
+}
---
+++ laShell/Assets/Scripts/Maquinas/Algoritmos/BehaviourTree/Trees/TestTree.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTree : MonoBehaviour
+{
+    [SerializeField]
+    GameObject mine;
+    List<BNode> FirstLayer;
+    List<BNode> SecondLayer;
+    NSecuence moveSecuence;
+    NSelector playerSelector;
+    NDecorator imNotInMine;
+    ActionNode isOnMineOne;
+    ActionNode isOnMineTwo;
+    ActionNode move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstLayer = new List<BNode>();
+        SecondLayer = new List<BNode>();
+        move = new ActionNode(MoveToMine);
+        isOnMineOne = new ActionNode(CheckInMine);
+        isOnMineTwo = new ActionNode(CheckInMine);
+        imNotInMine = new NDecorator(isOnMineTwo);
+        SecondLayer.Add(imNotInMine);
+        SecondLayer.Add(move);
+        moveSecuence = new NSecuence(SecondLayer);
+        FirstLayer.Add(isOnMineOne);
+        FirstLayer.Add(moveSecuence);
+        playerSelector = new NSelector(FirstLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerSelector.Evaluate();
+    }
+
+    public int CheckInMine()
+    {
+        if (transform.position == mine.gameObject.transform.position)
+        {
+            return 1;
+        }
+        return 0;
+    }
+    public int MoveToMine()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, mine.gameObject.transform.position, 0.05f);
+        return 1;
+    }
+}
